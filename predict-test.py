bb8436640d4c071341d531a9d02ef6af8aeecb94
Ann-M-R <ann.mariya308@gmail.com>
2025-01-24 22:10:06 +0530

    Dockerfile

--- predict-test.py
+++ predict-test.py
@@ -5,8 +5,8 @@ url = 'http://localhost:9696/predict'
 
 # %% Define the data to be sent for prediction
 data = {
-    "t1": 5.5,
-    "t2": 1.5,
+    "t1": 10.5,
+    "t2": 12.5,
     "hum": 78.5,
     "wind_speed": 24.0,
     "weather": "cloudy",
@@ -14,10 +14,10 @@ data = {
     "is_weekend": "weekday",
     "season": "winter",
     "month": 1,
-    "day": 6,
+    "day": 3,
     "hour": 13,
     "year": 2017,
-    "weekday": "Tuesday"
+    "weekday": "Sunday"
 }
 
 # %% 
